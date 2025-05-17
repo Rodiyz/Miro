7a803d1f5ec6d9ab56c49271cf7aa172ee3e25a0
Rodiyz <malinkin54ru@gmail.com>
2025-05-17 09:56:00 +0700

    route board

--- src/features/boards-list/boards-list.page.tsx
+++ src/features/boards-list/boards-list.page.tsx
@@ -1,7 +1,11 @@
+import { ROUTES } from "@/shared/model/routes";
+import { href, Link } from "react-router-dom";
+
 function BoardsListPage() {
   return (
     <div>
       <h1>Boards list</h1>
+      <Link to={href(ROUTES.BOARD, { boardId: "1" })}>Board 1</Link>
     </div>
   );
 }
